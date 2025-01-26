d3d3ba18c3f429d8a700f5d8db1b5db8fbc78284
jasmi <jasmine.yang5252@gmail.com>
2025-01-25 20:40:39 -0500

    .

--- src/window.py
+++ src/window.py
@@ -150,7 +150,7 @@ class Window:
         self.on_cleanup()
 
     def exit_render(self):
-        end_screen_image = pygame.image.load("../images/win_screen.png.")
+        end_screen_image = pygame.image.load("../images/win_screen.png")
         self._display_surf.blit(end_screen_image, end_screen_image.get_rect(topleft=(0, 0)))
         restart_button = pygame.image.load("../images/restart.png")
         self._display_surf.blit(restart_button, restart_button.get_rect(topleft=(100,200)))
